89e0588ca6b83c94d9862e7e40d54bbec7459222
Juan Feliciano <jp.feliciano10@uniandes.edu.co>
2022-11-09 23:42:21 -0500

    Escritura de tests unitarios para incrementar coverage.

--- src/test/java/de/rieckpil/courses/book/management/UserServiceTest.java
+++ src/test/java/de/rieckpil/courses/book/management/UserServiceTest.java
@@ -11,7 +11,9 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -37,7 +39,8 @@ class UserServiceTest {
 
     System.out.println(LocalDateTime.now());
 
-    try (MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(LocalDateTime.class)) {
+    try (MockedStatic<LocalDateTime> mockedLocalDateTime = Mockito.mockStatic(
+      LocalDateTime.class)) {
       mockedLocalDateTime.when(LocalDateTime::now).thenReturn(defaultLocalDateTime);
 
       System.out.println(LocalDateTime.now());
@@ -51,5 +54,18 @@ class UserServiceTest {
     }
 
     System.out.println(LocalDateTime.now());
+  }
+
+  @Test
+  void shouldReturnANewUserFromParameters() {
+    User user = new User();
+    user.setName("Juan F");
+    user.setEmail("jp.feliciano10@uniandes.edu.co");
+    user.setCreatedAt(LocalDateTime.now());
+    when(userRepository.findByNameAndEmail(anyString(), anyString())).thenReturn(null);
+    when(userRepository.save(any(User.class))).thenReturn(user);
+    User juanF = cut.getOrCreateUser(user.getName(), user.getEmail());
+    assertNotNull(juanF);
+    assertEquals(user.getEmail(), juanF.getEmail());
   }
 }
